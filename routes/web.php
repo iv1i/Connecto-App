19af1ab0088fe983c716b25410f5ac175b792cbd
ilya ilinyx <ilinyx_ilya@bk.ru>
2025-08-07 17:11:57 +0700

    develop - update web routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,24 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
+});
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('auth.register');
+})->name('register');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/chat', function () {
+        return view('chat.index');
+    });
+});
+
+// Handle auth redirects
+Route::get('/home', function () {
+    return redirect('/chat');
 });
