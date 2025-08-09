4528ea775e6000f24fcacef0d32a28a4ef17043a
ilya ilinyx <ilinyx_ilya@bk.ru>
2025-08-09 12:10:15 +0700

    develop - added new events - fix errors

---
+++ app/Events/ReactionEvent.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Message;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Contracts\Broadcasting\ShouldRescue;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ReactionEvent implements ShouldBroadcastNow, ShouldRescue
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $message;
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(Message $message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('reaction-add'),
+        ];
+    }
+}
